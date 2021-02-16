cf482e7fb7eca371cab131a97e5e0afab29ea816
Greg Smyth <grsmyth@deloitte.ca>
2021-02-16 16:29:29 -0500

    Errors...

--- src/Components/Body/index.js
+++ src/Components/Body/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { ReactComponent as Arrow } from "../../assets/images/email.svg";
+// import { ReactComponent as Arrow } from "../../assets/images/email.svg";
 import "./body.scss";
 
 function Body({ close }) {
